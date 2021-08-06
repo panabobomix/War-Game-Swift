3c722a907faa0e3a0d979d4ac7513fef9d4ea184
panabobomix <panabobomix@gmail.com>
2021-08-06 09:23:40 +0200

    Tutorial App
    App writing in Swift, using Xcode

---
+++ war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Grzegorz Lib on 05/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
